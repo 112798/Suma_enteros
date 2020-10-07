e00ea541ecadc42cacc200e4fcc88788f7d6e548
112798 <71934627+112798@users.noreply.github.com>
2020-10-07 17:13:22 -0500

    Agregando programas

---
+++ Programa_08.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int a = 5, b = 4, c = -1; 
+	
+	int suma_enteros = a + b + c;
+	int resta_enteros = a - b - c;
+	int producto_enteros = a * b * c;
+	
+	printf("Suma enteros %d\n", suma_enteros);
+	printf("Resta enteros %d\n", resta_enteros);
+	printf("Producto enteros %d\n", producto_enteros); 	
+}
